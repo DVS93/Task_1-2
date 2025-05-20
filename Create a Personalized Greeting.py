e98b59263e9a20445d5724c8f1dd6848695244e2
DVS93 <merai.dhiraj@gmail.com>
2025-05-20 14:33:45 +0530

    Add files via upload

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,10 @@
+# Task 2: Create a Personalized Greeting
+
+# Step 1: Take two input from the user
+First = (input("Enter your first name: "))
+Second = (input("Enter your second name: "))
+
+fullname = First + " " + Second
+
+# Step 2: Display the results
+print(f"Hello,{fullname}! Welcome to the Python program")
